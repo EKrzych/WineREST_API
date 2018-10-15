b7279cb3ca5167ee431d2dfb67c5118b71b3ccdb
edobkowski <edobkowski@gmail.com>
2018-10-15 13:51:34 +0200

    Testing code

--- src/main/java/com/codecool/wineREST/WineRestApplication.java
+++ src/main/java/com/codecool/wineREST/WineRestApplication.java
@@ -1,15 +1,27 @@
 package com.codecool.wineREST;
 
+import com.codecool.wineREST.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
-public class WineRestApplication {
+public class WineRestApplication implements CommandLineRunner {
+
+	@Autowired
+	private UserService userService;
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(WineRestApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		userService.createUser("edobkowski", "Eryk", "Dobkowski");
+		userService.getAll();
 	}
 }
