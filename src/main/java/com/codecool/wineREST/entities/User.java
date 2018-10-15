b7821e6d187bca0376c2c160f8bc0e6332d8a3bf
edobkowski <edobkowski@gmail.com>
2018-10-15 13:35:31 +0200

    Add User entity class

---
+++ src/main/java/com/codecool/wineREST/entities/User.java
@@ -0,0 +1,72 @@
+package com.codecool.wineREST.entities;
+
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Id;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class User {
+
+    @Id
+    @Column(name = "username")
+    private String username;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @ElementCollection
+    private Map<Wine, Integer> ratings = new HashMap<>();
+
+    public User(String username, String firstName, String lastName) {
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Map<Wine, Integer> getRatings() {
+        return ratings;
+    }
+
+    public void addRating(Wine wine, int rate) {
+        this.ratings.put(wine, rate);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(username, user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username);
+    }
+}
