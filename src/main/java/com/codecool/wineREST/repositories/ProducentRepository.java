0229ff5aae72ff1b0d60de06f4d1dde7cc42efcf
edobkowski <edobkowski@gmail.com>
2018-10-17 11:25:37 +0200

    Implement method for handling GET request method and archivisation on DELETE request method

--- src/main/java/com/codecool/wineREST/repositories/ProducentRepository.java
+++ src/main/java/com/codecool/wineREST/repositories/ProducentRepository.java
@@ -9,4 +9,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProducentRepository extends CrudRepository<Producent, Long> {
     Producent findByName(String name);
+    Producent findByIdProducent(long idProducent);
 }
