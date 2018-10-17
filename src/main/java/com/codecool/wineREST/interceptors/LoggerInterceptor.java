8afafdd7b073b77b697f4c6e868a8571e30313ea
Elzbieta Krzych <elzbieta.krzych@gmail.com>
2018-10-18 01:16:14 +0200

    Log to console, partially to file

--- src/main/java/com/codecool/wineREST/interceptors/LoggerInterceptor.java
+++ src/main/java/com/codecool/wineREST/interceptors/LoggerInterceptor.java
@@ -18,46 +18,9 @@ public class LoggerInterceptor extends HandlerInterceptorAdapter {
             Object handler) throws Exception {
         System.out.println("------------------------in prehandle");
         log.info("[preHandle][" + request + "]" + "[" + request.getMethod()
-                + "]" + request.getRequestURI() + getParameters(request));
+                + "]" + request.getRequestURI()+"WooooWWWWWWWW");
 
         return true;
-    }
-
-    private String getParameters(HttpServletRequest request) {
-        StringBuffer posted = new StringBuffer();
-        Enumeration<?> e = request.getParameterNames();
-        if (e != null) {
-            posted.append("?");
-        }
-        while (e.hasMoreElements()) {
-            if (posted.length() > 1) {
-                posted.append("&");
-            }
-            String curr = (String) e.nextElement();
-            posted.append(curr + "=");
-            if (curr.contains("password")
-                    || curr.contains("pass")
-                    || curr.contains("pwd")) {
-                posted.append("*****");
-            } else {
-                posted.append(request.getParameter(curr));
-            }
-        }
-        String ip = request.getHeader("X-FORWARDED-FOR");
-        String ipAddr = (ip == null) ? getRemoteAddr(request) : ip;
-        if (ipAddr!=null && !ipAddr.equals("")) {
-            posted.append("&_psip=" + ipAddr);
-        }
-        return posted.toString();
-    }
-
-    private String getRemoteAddr(HttpServletRequest request) {
-        String ipFromHeader = request.getHeader("X-FORWARDED-FOR");
-        if (ipFromHeader != null && ipFromHeader.length() > 0) {
-            log.debug("ip from proxy - X-FORWARDED-FOR : " + ipFromHeader);
-            return ipFromHeader;
-        }
-        return request.getRemoteAddr();
     }
 
     @Override
@@ -66,8 +29,9 @@ public class LoggerInterceptor extends HandlerInterceptorAdapter {
             throws Exception {
         if (ex != null){
             ex.printStackTrace();
+            log.error("OJJJJJJJJJJJJJJJJJJ");
         }
         System.out.println("--------------------------------aftercompletion");
-        log.info("[afterCompletion][" + request + "][exception: " + ex + "]");
+        log.info("[afterCompletion][" + request + "][exception: " + ex + "]" + "AAAAAAAAAAAAAAAAAA");
     }
 }
